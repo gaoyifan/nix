0e320837083836525024c87e64304d3426a5ccd6
Yifan Gao <git@yfgao.com>
2025-12-31 02:11:32 +0800

    feat: add binary cache workflow and update flake configuration for custom packages

--- flake.nix
+++ flake.nix
@@ -1,6 +1,11 @@
 {
   description = "Home Manager configuration of yifan";
 
+  nixConfig = {
+    extra-substituters = ["https://nix-cache.yfgao.net"];
+    extra-trusted-public-keys = ["nix-cache.yfgao.net-1:mSv/FykKK4oFZbX9JgD38D/me1+xJeAKsQ+STHiHVp4="];
+  };
+
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
@@ -34,6 +39,9 @@
         lazyssh = import ./packages/lazyssh.nix {inherit pkgs;};
       };
     in {
+      # Expose custom packages for CI/CD binary cache builds
+      legacyPackages.customPkgs = customPkgs;
+
       legacyPackages.homeConfigurations."${username}" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
         modules = [
