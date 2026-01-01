f9e93b53d64f0d8c3c3efd769bf4252ac2558665
Yifan Gao <git@yfgao.com>
2026-01-02 01:40:50 +0800

    fix(alias): add  --exclude "Mobile Documents" to ncdu

--- modules/shell/zsh-custom/alias/alias.plugin.zsh
+++ modules/shell/zsh-custom/alias/alias.plugin.zsh
@@ -1,3 +1,3 @@
 alias -g stat_='sort | uniq -c | sort -nk 1'
 alias ls='ls --color=auto'
-alias ncdu='ncdu -x --exclude "com~apple~CloudDocs"'
+alias ncdu='ncdu -x --exclude "com~apple~CloudDocs" --exclude "Mobile Documents"'
