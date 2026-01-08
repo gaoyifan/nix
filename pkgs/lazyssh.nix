de5bce2e4951c9e56c97ff13dad3053f854ca221
Yifan Gao <git@yfgao.com>
2026-01-09 02:55:47 +0800

    chore: update lazyssh rev

--- pkgs/lazyssh.nix
+++ pkgs/lazyssh.nix
@@ -7,7 +7,7 @@ pkgs.lazyssh.overrideAttrs (oldAttrs: {
   src = pkgs.fetchFromGitHub {
     owner = "gaoyifan";
     repo = "lazyssh";
-    rev = "f6258bf0d01c5d92a2c3363d97279dffe58fbf09";
-    hash = "sha256-wZRcIOWvX2k44zLjZ5wwrrAWQoMJu+aVGgqGoRVYQJ8=";
+    rev = "b6c16a921a0e851839b7bf2c4307bc09ce0d93b7";
+    hash = "sha256-B+5qo44rJ8w2dDwZgIBPFk+k216PLYPzUtI68oVa+K0=";
   };
 })
