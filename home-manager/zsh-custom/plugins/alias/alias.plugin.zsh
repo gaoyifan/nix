8e1f85a1bb3092822865646285062db848c5d3dc
Yifan Gao <git@yfgao.com>
2026-05-01 10:46:32 +0000

    feat(shell): add codex alias

--- home-manager/zsh-custom/plugins/alias/alias.plugin.zsh
+++ home-manager/zsh-custom/plugins/alias/alias.plugin.zsh
@@ -1,3 +1,4 @@
 alias -g stat_='sort | uniq -c | sort -nk 1'
+alias codex='nix run --accept-flake-config github:gaoyifan/nix#codex --'
 alias ls='ls -G --color=auto'
 alias ncdu='ncdu -x --exclude "com~apple~CloudDocs" --exclude "Mobile Documents"'
