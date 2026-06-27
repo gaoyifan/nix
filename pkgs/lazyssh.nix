4bedc041fce2b7136ea6671a6428f02de1305978
Yifan Gao <git@yfgao.com>
2026-06-27 18:29:40 +0000

    fix(lazyssh): stop forcing tssh udp mode

--- pkgs/lazyssh.nix
+++ pkgs/lazyssh.nix
@@ -7,7 +7,7 @@ pkgs.lazyssh.overrideAttrs (oldAttrs: {
   src = pkgs.fetchFromGitHub {
     owner = "gaoyifan";
     repo = "lazyssh";
-    rev = "ce9557389484380415af6c53dc84b420ee00b424";
-    hash = "sha256-rXTSHpdjzF4m8BbM5Q6MGzO52RiqQP+JS/Lo3oTyiIM=";
+    rev = "c516a03f206992169f6b27b9ba08b2aa5bede662";
+    hash = "sha256-/fCdihZ7PPqCDrrTtmRtAD+J1VGmm15ol4Qm0Fpiv+w=";
   };
 })
