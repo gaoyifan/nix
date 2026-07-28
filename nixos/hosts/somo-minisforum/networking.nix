4622de4753849e9c35caa7a875e7bae16fdb6a86
Yifan Gao <git@yfgao.com>
2026-07-28 16:42:13 +0800

    fix(somo-minisforum): bootstrap wg-iplc endpoint DNS

--- nixos/hosts/somo-minisforum/networking.nix
+++ nixos/hosts/somo-minisforum/networking.nix
@@ -230,10 +230,14 @@ in {
     DNS = ["1.1.1.1" "1.0.0.1"];
     Domains = ["~."];
   };
-  services.resolved.dnsDelegates.cjia = {
-    Delegate = {
+  services.resolved.dnsDelegates = {
+    cjia.Delegate = {
       DNS = ["100.65.1.254"];
       Domains = ["cjia.gaof.net"];
+    };
+    wgIplcEndpoint.Delegate = {
+      DNS = ["223.5.5.5" "223.6.6.6"];
+      Domains = ["int.automesh.org"];
     };
   };
 
