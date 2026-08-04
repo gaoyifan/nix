77a57684899f8dae4d73c613a5f812cd3333a90e
Yifan Gao <git@yfgao.com>
2026-08-04 23:25:03 +0800

    feat(el2): configure split DNS resolution

--- nixos/hosts/el2/default.nix
+++ nixos/hosts/el2/default.nix
@@ -3,6 +3,7 @@
     ./disk-config.nix
     ./hardware-configuration.nix
     ./networking.nix
+    ./services.nix
     ./tailscale.nix
     ./wg-iplc.nix
     ./wg0.nix
---
+++ nixos/hosts/el2/services.nix
@@ -0,0 +1,26 @@
+{...}: {
+  imports = [../../optional/diverge.nix];
+
+  services.diverge.enable = true;
+
+  services.resolved.settings.Resolve = {
+    DNS = [
+      "1.1.1.1"
+      "1.0.0.1"
+    ];
+    Domains = ["~."];
+  };
+  services.resolved.dnsDelegates = {
+    cjia.Delegate = {
+      DNS = ["100.65.1.254"];
+      Domains = ["cjia.gaof.net"];
+    };
+    wgIplcEndpoint.Delegate = {
+      DNS = [
+        "223.5.5.5"
+        "223.6.6.6"
+      ];
+      Domains = ["int.automesh.org"];
+    };
+  };
+}
