b9bd1fc28f6ae36362459b0739e58abab7df1b91
Yifan Gao <git@yfgao.com>
2026-08-13 12:47:51 +0800

    feat(htop): count only e-prefixed network interfaces

--- pkgs/htop.nix
+++ pkgs/htop.nix
@@ -9,12 +9,12 @@
 # "/sys: No sensors at /sys/class/hwmon." to stderr on every launch.
 {pkgs}:
 (pkgs.htop.override {sensorsSupport = false;}).overrideAttrs {
-  version = "3.5.1-fork-472cee72";
+  version = "3.5.1-fork-74ec7f59";
 
   src = pkgs.fetchFromGitHub {
     owner = "gaoyifan";
     repo = "htop";
-    rev = "472cee728dd6142bab417d78b791f4d4abd57c2d";
-    hash = "sha256-NzxP6U8YxTTt5ctp2aTWXtzTtD3ACKFTrRVz2gDGqX8=";
+    rev = "74ec7f59c7391f72c140fa2d101d1192ec985a35";
+    hash = "sha256-X7GOJdxW/pjdLl6tN14xZPG87gkOwyF3sbr20wX2AUQ=";
   };
 }
