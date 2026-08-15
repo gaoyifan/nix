7120bf2815042479123734a26acbe638c4b14d46
Yifan Gao <git@yfgao.com>
2026-08-15 08:20:38 +0800

    refactor(el2): migrate mutagen sync to nspawn

--- nixos/hosts/el2/mutagen-sync.nix
+++ nixos/hosts/el2/mutagen-sync.nix
@@ -2,36 +2,60 @@
   lib,
   pkgs,
   ...
-}: {
+}: let
+  inherit (import ../../common/ssh-keys.nix) sshKeys;
+in {
   networking.edgeFirewall.publicTcpPorts = ["2221"];
 
-  virtualisation.oci-containers.containers.mutagen-sync = {
-    autoStart = false;
-    image = "lscr.io/linuxserver/openssh-server:latest";
-    environment = {
-      PUID = "1000";
-      PGID = "1000";
-      USER_NAME = "syncd";
-      PASSWORD_ACCESS = "false";
-      SUDO_ACCESS = "false";
-      LISTEN_PORT = "2221";
-      TZ = "Asia/Shanghai";
+  containers.mutagen-sync = {
+    ephemeral = true;
+    bindMounts = {
+      "/etc/ssh/ssh_host_ed25519_key".hostPath = "/pool1/services/mutagen-sync/ssh/ssh_host_ed25519_key";
+      "/etc/ssh/ssh_host_rsa_key".hostPath = "/pool1/services/mutagen-sync/ssh/ssh_host_rsa_key";
+      "/home/syncd" = {
+        hostPath = "/pool1/services/mutagen-sync/config";
+        isReadOnly = false;
+      };
+      "/data" = {
+        hostPath = "/pool1/services/mutagen-sync/data";
+        isReadOnly = false;
+      };
     };
-    volumes = [
-      "/pool1/services/mutagen-sync/config:/config"
-      "/pool1/services/mutagen-sync/data:/data"
-    ];
-    extraOptions = ["--network=host"];
+    config = {pkgs, ...}: {
+      networking.firewall.enable = false;
+
+      users = {
+        groups.syncd.gid = 1000;
+        users.syncd = {
+          isNormalUser = true;
+          uid = 1000;
+          group = "syncd";
+          openssh.authorizedKeys.keys = sshKeys;
+        };
+      };
+
+      services.openssh = {
+        enable = true;
+        ports = [2221];
+        settings = {
+          AllowTcpForwarding = false;
+          KbdInteractiveAuthentication = false;
+          PasswordAuthentication = false;
+        };
+      };
+
+      environment.systemPackages = [pkgs.rsync];
+      system.stateVersion = "26.05";
+    };
   };
 
-  systemd.services.podman-mutagen-sync = {
+  systemd.services."container@mutagen-sync" = {
     wantedBy = ["el2-services.target"];
     requires = ["mount-el2-encrypted-datasets.service"];
     after = ["mount-el2-encrypted-datasets.service"];
     preStart = ''
-      ${lib.getExe' pkgs.coreutils "install"} -d -m 0755 -o 1000 -g 1000 \
-        /pool1/services/mutagen-sync/config \
-        /pool1/services/mutagen-sync/data
+      ${lib.getExe' pkgs.coreutils "install"} -d -m 0700 -o 1000 -g 1000 /pool1/services/mutagen-sync/config
+      ${lib.getExe' pkgs.coreutils "install"} -d -m 0755 -o 1000 -g 1000 /pool1/services/mutagen-sync/data
     '';
   };
 }
