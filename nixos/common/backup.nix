020c5898135ea143280bb782540fd3caea8c6ec1
Yifan Gao <git@yfgao.com>
2026-08-28 04:26:15 +0800

    refactor(nixos): make common restic backup unconditional

--- nixos/common/backup.nix
+++ nixos/common/backup.nix
@@ -7,13 +7,6 @@
   cfg = config.services.resticBackup;
 in {
   options.services.resticBackup = {
-    enable = lib.mkOption {
-      type = lib.types.bool;
-      default = config.services.tailscale.enable;
-      defaultText = lib.literalExpression "config.services.tailscale.enable";
-      description = "Whether to enable the Restic system backup.";
-    };
-
     extraPaths = lib.mkOption {
       type = lib.types.listOf lib.types.str;
       default = [];
@@ -27,50 +20,48 @@ in {
     };
   };
 
-  config = lib.mkIf cfg.enable {
-    services.restic.backups.system = {
-      environmentFile = "/home/${username}/.config/restic/env";
-      paths = lib.sort builtins.lessThan (
-        [
-          "/etc"
-          "/home"
-          "/opt"
-          "/root"
-          "/srv"
-          "/var/lib/tailscale"
-        ]
-        ++ cfg.extraPaths
-      );
-      exclude =
-        [
-          ".antigravity-server"
-          ".bun"
-          ".cache"
-          ".cursor-server"
-          ".npm"
-          ".rustup"
-          ".venv"
-          "/home/linuxbrew"
-          "/home/${username}/.cargo/registry"
-          "/home/${username}/.local"
-          "/home/${username}/.syncd-dotfiles"
-          "/home/${username}/Sync"
-          "/home/${username}/go/pkg/mod"
-          "/home/${username}/nix"
-          "/home/${username}/src"
-          "/home/${username}/tmp"
-          "__pycache__"
-          "cache"
-          "log"
-          "node_modules"
-          "target"
-        ]
-        ++ cfg.extraExcludes;
-      timerConfig = {
-        OnCalendar = "daily";
-        RandomizedDelaySec = "1h";
-        Persistent = true;
-      };
+  config.services.restic.backups.system = {
+    environmentFile = "/home/${username}/.config/restic/env";
+    paths = lib.sort builtins.lessThan (
+      [
+        "/etc"
+        "/home"
+        "/opt"
+        "/root"
+        "/srv"
+        "/var/lib/tailscale"
+      ]
+      ++ cfg.extraPaths
+    );
+    exclude =
+      [
+        ".antigravity-server"
+        ".bun"
+        ".cache"
+        ".cursor-server"
+        ".npm"
+        ".rustup"
+        ".venv"
+        "/home/linuxbrew"
+        "/home/${username}/.cargo/registry"
+        "/home/${username}/.local"
+        "/home/${username}/.syncd-dotfiles"
+        "/home/${username}/Sync"
+        "/home/${username}/go/pkg/mod"
+        "/home/${username}/nix"
+        "/home/${username}/src"
+        "/home/${username}/tmp"
+        "__pycache__"
+        "cache"
+        "log"
+        "node_modules"
+        "target"
+      ]
+      ++ cfg.extraExcludes;
+    timerConfig = {
+      OnCalendar = "daily";
+      RandomizedDelaySec = "1h";
+      Persistent = true;
     };
   };
 }
