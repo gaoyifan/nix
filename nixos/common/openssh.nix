25db0fe599c2f868264dcc8c565c06ccac4d6e72
Yifan Gao <git@yfgao.com>
2026-08-31 20:12:33 +0800

    fix(openssh): limit unauthenticated connections per source

--- nixos/common/openssh.nix
+++ nixos/common/openssh.nix
@@ -1,5 +1,21 @@
 # SSH server settings shared by all hosts.
-{config, ...}: {
+{
+  config,
+  lib,
+  ...
+}: let
+  ustcAddresses = lib.filter (line: line != "" && !lib.hasPrefix "#" line) (
+    lib.splitString "\n" (builtins.readFile ../../pkgs/nft-geo-sets/ustc.txt)
+  );
+  penaltyExemptAddresses =
+    ustcAddresses
+    ++ [
+      "10.0.0.0/8"
+      "100.64.0.0/10"
+      "172.16.0.0/12"
+      "192.168.0.0/16"
+    ];
+in {
   services.fail2ban.enable = config.networking.firewall.enable || config.networking.nftables.enable;
 
   services.openssh = {
@@ -8,6 +24,9 @@
     settings = {
       PasswordAuthentication = false;
       KbdInteractiveAuthentication = false;
+      PerSourceMaxStartups = 3;
+      PerSourceNetBlockSize = "22:48";
+      PerSourcePenaltyExemptList = lib.concatStringsSep "," penaltyExemptAddresses;
     };
   };
 }
