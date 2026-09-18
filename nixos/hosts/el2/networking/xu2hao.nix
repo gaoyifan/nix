84bbd2274f56529794cd5468893f775c04788665
Yifan Gao <git@yfgao.com>
2026-09-19 00:19:18 +0800

    feat(el2): forward TCP and UDP ports 10300-10309 to xuhao

--- nixos/hosts/el2/networking/xu2hao.nix
+++ nixos/hosts/el2/networking/xu2hao.nix
@@ -14,7 +14,7 @@ in {
     content = ''
       chain prerouting-dnat {
         type nat hook prerouting priority dstnat; policy accept;
-        iifname "${chinanetInterface}" ip daddr ${el2WanAddresses.chinanet.ipv4} meta l4proto { tcp, udp } th dport 10300 dnat ip to 100.64.2.30
+        iifname "${chinanetInterface}" ip daddr ${el2WanAddresses.chinanet.ipv4} meta l4proto { tcp, udp } th dport 10300-10309 dnat ip to 100.64.2.30
       }
 
       chain force-chinanet {
